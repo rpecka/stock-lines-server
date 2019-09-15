42f02b09da452c4030ecfe0707d6f94a1ef31743
Daniel Cheriyan <echeriyan72@gmail.com>
2019-09-14 23:41:52 -0400

    converted dates to epoch time

--- S&P Historical Dict.js
+++ S&P Historical Dict.js
@@ -2,8 +2,15 @@ var csv = require('fast-csv');
 var dict = {};
 var data = [];
 
-csv.fromPath('downloads/S&P 500 Historical Data (3).csv')
+function converttoEpoch (day){
+let now = new Date(day);
+console.log(now);
+let present = now.getTime();
+return present;
+}
+
+csv.fromPath('C:/Users/Daniel/Downloads/S&P 500 Historical Data (3).csv')
   .on('data', function(data) {
-  dict[data[0]] = data[1];
+  dict[converttoEpoch(data[0])] = data[1];
   console.log(dict);
   });
